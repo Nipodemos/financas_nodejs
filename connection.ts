3465ec81f5de13c30dc88695b8c2f4747d641f6c
Nipodemos <nipodemos13@gmail.com>
2023-04-08 13:12:35 -0300

    inicialização do db é em outro arquivo agora

---
+++ connection.ts
@@ -0,0 +1,42 @@
+import Surreal from "surrealdb.js";
+import dotEnvExtended from "dotenv-extended";
+dotEnvExtended.load();
+const db = new Surreal(process.env.SURREAL_DB_HOST);
+
+export async function initDB() {
+  try {
+    console.log("Initializing database...");
+    if (
+      !process.env.SURREAL_DB_USER ||
+      !process.env.SURREAL_DB_PASSWORD ||
+      !process.env.SURREAL_DB_HOST
+    ) {
+      throw new Error("process.env.SURREAL_DB_USERNAME or DB_PASSWORD not set");
+    }
+    await db
+      .connect(process.env.SURREAL_DB_HOST)
+      .then(() => {
+        console.log("Connected to database");
+      })
+      .catch((err) => {
+        console.log("Error connecting to database", err);
+      });
+
+    await db
+      .signin({
+        user: process.env.SURREAL_DB_USER,
+        pass: process.env.SURREAL_DB_PASSWORD,
+      })
+      .then((res) => {
+        console.log("Signed in to database", res);
+      })
+      .catch((err) => {
+        console.log("Error signing in to database", err);
+      });
+
+    await db.use("test", "test");
+  } catch (err) {
+    console.error(err);
+  }
+}
+export default db;
