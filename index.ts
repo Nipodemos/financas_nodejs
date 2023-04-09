74c69ffaa044ca2c9259bae531925fceccbf4ad2
Nipodemos <nipodemos13@gmail.com>
2023-04-09 15:47:26 -0300

    tendo certeza que o banco de dados está conectado antes de iniciar o servidor

--- index.ts
+++ index.ts
@@ -3,25 +3,36 @@ import cors from "cors";
 import dotEnvExtended from "dotenv-extended";
 import { initDB } from "./connection";
 dotEnvExtended.load();
-initDB();
+initDB()
+  .then((estaConectado) => {
+    if (estaConectado) {
+      console.log("Conectado ao banco de dados");
 
-const app = express();
-const router = express.Router();
+      const app = express();
+      const router = express.Router();
 
-router.get("/", async (req, res) => {
-  res.send("All systems operational");
-});
+      router.get("/", async (req, res) => {
+        res.send("All systems operational");
+      });
 
-router.get("/criar_usuario", (req, res) => {
-  res.send("Criando usuário");
-});
+      router.get("/criar_usuario", (req, res) => {
+        res.send("Criando usuário");
+      });
 
-router.get("/alterar_usuario", (req, res) => {
-  res.send("Alterando usuário");
-});
+      router.get("/alterar_usuario", (req, res) => {
+        res.send("Alterando usuário");
+      });
 
-app.use(cors());
-app.use(router);
-app.listen(process.env.PORT, () => {
-  console.log("Listening on port " + process.env.PORT);
-});
+      app.use(cors());
+      app.use(router);
+      app.listen(process.env.PORT, () => {
+        console.log("Listening on port " + process.env.PORT);
+      });
+    } else {
+      console.log("Não foi possível conectar ao banco de dados");
+    }
+  })
+  .catch((erro) => {
+    console.log("Erro ao conectar ao banco de dados");
+    console.log(erro);
+  });
